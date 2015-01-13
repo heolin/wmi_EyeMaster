e0ad20e7557aca4c36dbb370f33ef074163590b6
heolin123 <heolin123@gmail.com>
2015-01-13 22:56:08 +0100

    Changed frame_processor.py so it detect eyes and nose in tuple form.

---
+++ Makefile
@@ -0,0 +1,2 @@
+debug:
+	./eye_server.py -hc haarcascade_frontalface_ext.xml -ec haarcascade_eye_ext.xml -nc Nariz_nuevo_20stages.xml -d
--- frame_processor.py
+++ frame_processor.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import zmq
 import argparse
+import numpy as np
 from operator import attrgetter
 
 class FrameProcessor:
@@ -9,18 +10,26 @@ class FrameProcessor:
         return None
 
     def get_head_angle(self, faces, eyes, noses):
-        if len(noses) > 0:
-            print(str(noses[0]))
-            nose = min(noses, key=attrgetter('ey'))
-            left_eye = min(eyes, key=attrgetter('ex'))
-            right_eye = max(eyes, key=attrgetter('ex'))
+        for (x, y, w, h) in faces:
+            face = (x, y, w, h)
+            if len(noses) > 0:
+                nose = (noses[face][0][0], noses[face][0][1],\
+                        noses[face][0][2], noses[face][0][2])
+                eyes = [(eye[0], eye[1], eye[2], eye[3]) for eye in eyes[face]]
 
-            left_eye_center = left_eye.ex + left_eye.ew
-            right_eye_center = right_eye.ex + right_eye.ew
-            nose_center = nose.ex + nose.ew
+                if eyes[0][0] > eyes[1][0]:
+                    left_eye = eyes[0]
+                    right_eye = eyes[1]
+                else:
+                    left_eye = eyes[1]
+                    right_eye = eyes[0]
 
-            result = (right_eye_center - left_eye_center)/2 - nose_center
-            print(str(result))
+                left_eye_center = left_eye[0] + left_eye[2]
+                right_eye_center = right_eye[0] + right_eye[2]
+                nose_center = nose[0] + nose[2]
+
+                result = (right_eye_center - left_eye_center)/2 - nose_center
+                print result
             return result
 
         return None
